6bd428a4a71a9bdd9ad2761ec42b8154c44f5a29
Kevin Buffardi <kbuffardi@csuchico.edu>
2020-09-22 14:49:41 -0700

    Adding starter code

--- main.cpp
+++ main.cpp
@@ -4,6 +4,25 @@ using namespace std;
 
 int main()
 {
+  char symbol;
+  int repeats;
+  
+  while( repeats > 0 )
+  {
+    cout<<"Symbol:\n";
+    cin>>symbol;
+    cout<<"Repeats:\n";
+    cin>>repeats;
+  }
+  
+  for(int counter1 = 0; counter1 < repeats; counter1++ )
+  {
+    for(int counter2 = 0; counter2 < repeats; counter2++ )
+    {
+      cout<<symbol;
+    }
+    cout<<endl; 
+  }
   
   return 0;
 }
